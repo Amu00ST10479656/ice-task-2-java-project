a7f027d191d0e4d3ae8d109f2912c753e6e3c611
Amukelisiwe Mtolo <ST10479656@myemeris.edu.za>
2026-04-03 12:00:07 +0200

    Initial Project setup

---
+++ BasketballQuizGame/src/basketballquizgame/BasketballQuizGame.java
@@ -0,0 +1,17 @@
+
+package basketballquizgame;
+
+/**
+ *
+ * @author amuke
+ */
+public class BasketballQuizGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
